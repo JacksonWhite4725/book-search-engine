35246b7f146ce398e2da783cc0404aef8eb4c0d0
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-29 22:20:51 -0700

    Added typeDefs and fixed typo in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,7 +47,7 @@ const resolvers = {
             }
             throw new AuthentificationError('You must login first');
         },
-        deleteBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
